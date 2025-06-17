683ac0ed9d2059216f8d50d910da88df21cba40d
YasirKhan231 <yk4987746@gmail.com>
2025-06-17 21:22:09 +0530

    text correct

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Yasir Portfolio website",
+  title: "Yasir Portfolio ",
   description: "Portfolio website created in nextjs ",
 };
 
